962d7cbe2d83d75dfe13e8addfebbffbb3343d06
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-07-31 20:47:42 +0100

    array challenges

--- Js workspace/challenges/09-arrays.js
+++ Js workspace/challenges/09-arrays.js
@@ -1,11 +1,45 @@
-console.log(students);
+// console.log(students);
 
-// Updated students
-// 1. Add role 'student' property to each object using MAP method
-// 2. Assign to "updatedStudents" variable and log
+// // Updated students
+// // 1. Add role 'student' property to each object using MAP method
+// // 2. Assign to "updatedStudents" variable and log
 
-const updatedStudents = students.map(function(student) {
-    return student
+// const updatedStudents = students.map(function(student) {
+//     student.role = 'student'
+//     return student
+// })
+
+// console.log(updatedStudents)
+
+
+// // High scores
+// // 1. filter array and return only scores >= 80
+// // 2. assign to 'highScores' variable and log 
+
+// const highScores = students.filter(function(student) {
+    
+//     if (student.score >= 90) {
+//         return student
+//     }
+// })
+
+// console.log(highScores)
+
+
+// specific id
+// 1. find specific id in array
+// 2. assign to 'specificId' variable and log
+
+const specificId = students.find(function(student) {
+    return student.id === 2
 })
 
-console.log(updatedStudents)
+console.log(specificId)
+
+
+
+
+// average score
+// 1. sum up all student.score values with reduce
+// 2. divide by the length of the students array
+// 3. assign to 'averageScore'
